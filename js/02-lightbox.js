3a8b1eaf839f14574a5d9e09e3421e149da9c13b
VarshavetsAleksandr <varshavetsaleksandr@gmail.com>
2023-08-05 18:53:09 +0300

    pre-final hw 07

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Створення і рендер розмітки на підставі масиву даних galleryItems і
+// наданого шаблону елемента галереї.Використовуй готовий код з першого завдання.
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
+    </li>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+// gallery.addEventListener("click", handlerClickImg);
+
+// function handlerClickImg(evt) {
+//   if (evt.target === evt.currentTarget) {
+//     return;
+//   }
+//   evt.preventDefault();
+
+//   const lightbox = new SimpleLightbox(".gallery a", {
+//     captionsData: "alt",
+//     captionDelay: 250,
+//     docClose: true,
+//   });
+// }
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
